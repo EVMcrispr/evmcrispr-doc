3f62ac2cbc231a9a376871676491996b66239f52
geleeroyale <krassvs@gmail.com>
2022-04-02 18:28:18 +0200

    init

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/geleeroyale/Github/evmcrispr-doc/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/geleeroyale/Github/evmcrispr-doc/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/geleeroyale/Github/evmcrispr-doc/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
+  require('/Users/geleeroyale/Github/evmcrispr-doc/src/css/custom.css'),
+];
