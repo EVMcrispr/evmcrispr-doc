3d24c49adfbaba2b5758841bdcbcbe48f9e1612b
geleeroyale <krassvs@gmail.com>
2022-04-13 15:24:20 +0200

    remove generated folder .docusaurus from git cache

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/mitch/github/evmcrispr-doc/node_modules/infima/dist/css/default/default.css'),
-  require('/home/mitch/github/evmcrispr-doc/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/mitch/github/evmcrispr-doc/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-  require('/home/mitch/github/evmcrispr-doc/src/css/custom.css'),
-];
